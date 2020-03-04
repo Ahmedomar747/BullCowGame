e3100b0f0d205be30e8e29c8bf15ba0a8b36282a
Ahmedomar747 <42421885+Ahmedomar747@users.noreply.github.com>
2020-03-04 23:18:43 +0200

    Game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -3,10 +3,46 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    UE_LOG(LogTemp, Warning, TEXT("Your message"));
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cow Game."));
+    PrintLine(TEXT("You have 5 Guesses!"));
+
+    InitGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    HiddenWord.Len();
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("YOU HAVE WON!"));
+    }
+    else if (Input.Len() == HiddenWord.Len())
+    {
+
+    }
+    else
+    {
+
+    }
+}
+void UBullCowCartridge::InitGame()
+{
+    // Initialize Game Variables
+    HiddenWord = TEXT("plain");
+    Guesses = 5;
 
 }
+
+struct BullCowCount
+{
+    int32 bulls = 0;
+    int32 cows = 0;
+
+    BullCowCount()
+    {
+
+    }
+};
